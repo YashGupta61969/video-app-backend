42e5af1f56c67fc6fb5130d4e6f0caf5ef739944
Yash Gupta <yashgupta61969@gmail.com>
2023-01-10 20:20:00 +0530

    added get route to fetch converted videos

--- app.js
+++ app.js
@@ -24,12 +24,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 app.use(express.json())
 
-// app.get('/', (req, res) => {
-
-//     const videoFiles = fs.readFileSync('videos')
-
-//     res.send('ssss')
-// })
+app.get('/', async (req, res) => {
+    try {
+        const videos = await db.videos.findAll({})
+        res.send(videos)
+    } catch (error) {
+        res.send(error)
+    }
+})
 
 
 app.post('/', async (req, res) => {
@@ -42,64 +44,65 @@ app.post('/', async (req, res) => {
                     res.status(500).send({ message: 'Internal Sevrer error' })
                 } else {
 
-                    const removeRawVideo = ()=>{
+                    const removeRawVideo = () => {
                         fs.readdir('raw-videos', (err, files) => {
                             if (err) throw err;
-                          
+
                             for (const file of files) {
-                              fs.unlink(path.join('raw-videos', file), (err) => {
-                                if (err) throw err;
-                              });
+                                fs.unlink(path.join('raw-videos', file), (err) => {
+                                    if (err) throw err;
+                                });
                             }
-                          });
+                        });
                     }
 
-                    const convertVideo = ()=>{
-                        files.map(f=>{
+                    const convertVideo = () => {
+                        files.map(f => {
                             const command = ffmpeg({ source: `./raw-videos/${f}` })
                             command
-                            .format('mp4')
-                            .videoFilters({
-                                filter: 'drawtext',
-                                options: {
-                                    text: 'What Is The Longest River In The World ?',
-                                    fontsize: '(h/20)',
-                                    fontcolor: 'white',
-                                    box: 1,
-                                    boxborderw: 10,
-                                    boxcolor: 'black@0.3',
-                                    x: 10,
-                                    y: 10,
-                                }
-                            })
-                            .outputOptions(['-frag_duration 100', '-movflags frag_keyframe+empty_moov', '-pix_fmt yuv420p'])
-                            .output(`converted-videos/${f}`)
-                            .on('error', function (err, stdout, stderr) {
-                                console.log('err',err.message)
-                                console.log('stdout',stdout)
-                                console.log('stderr',stderr)
-                            }).run();
+                                .format('mp4')
+                                .videoFilters({
+                                    filter: 'drawtext',
+                                    options: {
+                                        text: 'What Is The Longest River In The World ?',
+                                        fontsize: '(h/20)',
+                                        fontcolor: 'white',
+                                        box: 1,
+                                        boxborderw: 10,
+                                        boxcolor: 'black@0.3',
+                                        x: 10,
+                                        y: 10,
+                                    }
+                                })
+                                .outputOptions(['-frag_duration 100', '-movflags frag_keyframe+empty_moov', '-pix_fmt yuv420p'])
+                                .output(`converted-videos/${f}`)
+                                .on('error', function (err, stdout, stderr) {
+                                    console.log('err', err.message)
+                                    console.log('stdout', stdout)
+                                    console.log('stderr', stderr)
+                                }).run();
                         });
-                }
-
-                const data = {
-                    video: `http://localhost:8000/converted-videos\\${req.files.video.name.replace(/ /g, '')}`,
-                    name: req.files.name
-                }
-            
-            
-                db.videos.create(data).then(data => {
-                    // removeRawVideo()
-                    res.send({ status: 'success', ...data.dataValues })
-                }).catch(err => {
-                    console.log(err)
-                    res.status(500).send({ status: 'error', message: 'Internal Server Error' })
-                })
-                convertVideo()
                     }
-                })
-            }
-        });
+
+                    const data = {
+                        // To Be Replaced With The Deployed Url
+                        video: `http://localhost:8000/converted-videos\\${req.files.video.name.replace(/ /g, '')}`,
+                        name: req.files.name
+                    }
+
+
+                    db.videos.create(data).then(data => {
+                        // removeRawVideo()
+                        res.send({ status: 'success', ...data.dataValues })
+                    }).catch(err => {
+                        console.log(err)
+                        res.status(500).send({ status: 'error', message: 'Internal Server Error' })
+                    })
+                    convertVideo()
+                }
+            })
+        }
+    });
 })
 
 
