91c698c038f777fdefff1b6fef38165df74c5d77
sakshithakre20001 <sakshithakre20001@gmail.com>
2023-01-19 20:28:10 +0530

    replace express fileupload with multer

--- app.js
+++ app.js
@@ -1,134 +1,179 @@
-require('dotenv').config()
+require("dotenv").config();
 require("./db");
-const express = require('express')
-const ffmpeg = require('fluent-ffmpeg');
-const fs = require('fs')
-const fileUpload = require('express-fileupload');
-const cors = require('cors');
-const path = require('path');
-const { uploadBytes, ref, getDownloadURL } = require('firebase/storage');
-const { storage, database } = require('./firebase');
-const { addDoc, collection } = require('firebase/firestore');
-const port = process.env.PORT || 8000
+const express = require("express");
+const ffmpeg = require("fluent-ffmpeg");
+const fs = require("fs");
+// const fileUpload = require('express-fileupload');
+const multer = require("multer");
+const cors = require("cors");
+const path = require("path");
+const { uploadBytes, ref, getDownloadURL } = require("firebase/storage");
+const { storage, database } = require("./firebase");
+const { addDoc, collection } = require("firebase/firestore");
+const port = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(
-    fileUpload({
-        limits: {
-            fileSize: 50000000, // Around 50MB
-        },
-        abortOnLimit: true,
-    })
-);
+// const multerStorage = multer.diskStorage({
+//   destination: "raw-videos",
+//   filename: (req, file, cb) => {
+//     cb(
+//       null,
+//        Date.now()
+//       );
+//     },
+// });
 
-app.use('/video-app/converted-videos', express.static('./converted-videos'));
+const upload = multer({ dest: 'raw-videos/' }).single('video')
+
+// const upload = multer({ storage: multerStorage }).single("video");
+// app.use(
+//     fileUpload({
+//         limits: {
+//             fileSize: 50000000, // Around 50MB
+//         },
+//         abortOnLimit: true,
+//     })
+// );
+
+app.use("/video-app/converted-videos", express.static("./converted-videos"));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 
+app.get("/video-app", async (req, res) => {
+  try {
+    // const videos = await db.videos.findAll({})
+    res.send({ messgae: "success API Working" });
+  } catch (error) {
+    res.send({ error });
+  }
+});
 
+app.post("/video-app", upload, async (req, res) => {
+  const name = new Date().getTime();
 
-app.get('/video-app', async (req, res) => {
-    try {
-        const videos = await db.videos.findAll({})
-        res.send({ messgae: 'success', videos })
-    } catch (error) {
-        res.send(error)
-    }
-})
+  // Reads the raw-videos dir after creation
+  fs.readdir("./raw-videos/", (err, files) => {
+    if (err) {
+      return res
+        .status(500)
+        .send({ message: "Internal Sevrer error", error5: err });
+    } else {
+      // converts the raw video to add text and push it to converted-videos dir
+      const command = ffmpeg({ source: `./raw-videos/${files[0]}` });
+      command
+        .format("mp4")
+        .videoFilters({
+          filter: "drawtext",
+          options: {
+            text: "What Is The Longest River In The World ?",
+            fontsize: "(h/25)",
+            fontcolor: "white",
+            box: 1,
+            boxborderw: 10,
+            fontfile:'./Dirtyboy-BxYl.ttf',
+            boxcolor: "black@0.4",
+            x: 10,
+            y: 10,
+          },
+        })
+        .outputOptions([
+          "-frag_duration 100",
+          "-movflags frag_keyframe+empty_moov",
+          "-pix_fmt yuv420p",
+        ])
+        .output(`converted-videos/${name}.mp4`)
+        .on("error", (err, stdout, stderr) => {
+          res.send({ stderr });
+        })
+        .on("progress", (progress) => {
+          progress.percent > 99 &&
+            fs.readdir("./converted-videos", async (err, files) => {
+              if (err) {
+                return res
+                  .status(500)
+                  .send({ message: "Internal Sevrer error", error: err });
+              } else {
+                const file = files[files.length - 1];
+                fs.readFile(
+                  `./converted-videos/${file}`,
+                  async (error, item) => {
+                    if (error) {
+                      return res
+                        .status(500)
+                        .send({ message: "Internal Sevrer error", error });
+                    } else {
+                      const refrence = ref(storage, `${file}`);
+                      const snap = await uploadBytes(refrence, item);
+                      const url = await getDownloadURL(
+                        ref(storage, snap.ref.fullPath)
+                      );
+                      await addDoc(collection(database, "videos"), {
+                        media: url,
+                        name,
+                      });
 
-app.post('/video-app', async (req, res) => {
-console.log(req.files.video)    
+                      // Removes Previously Added Videos
+                      fs.readdir("./raw-videos", (err, files) => {
+                        if (err) {
+                          return res.status(500).send({
+                            message: "Internal Sevrer error",
+                            error1: err,
+                          });
+                        }
 
-    const name = new Date().getTime();
+                        for (const file of files) {
+                          fs.unlink(path.join("raw-videos", file), (err) => {
+                            if (err) {
+                              return res.status(500).send({
+                                message: "Internal Sevrer error",
+                                error2: err,
+                              });
+                            }
+                          });
+                        }
+                      });
 
-    // Removes Previously Added Videos
-    fs.readdir('raw-videos', (err, files) => {
-        if (err) {
-            return res.status(500).send({ message: 'Internal Sevrer error' })
-        }
+                      // Removes Previously converted Videos
+                      fs.readdir("converted-videos", (err, files) => {
+                        if (err) {
+                          return res.status(500).send({
+                            message: "Internal Sevrer error",
+                            error3: err,
+                          });
+                        }
 
-        for (const file of files) {
-            fs.unlink(path.join('raw-videos', file), (err) => {
-                if (err) {
-                    return res.status(500).send({ message: 'Internal Sevrer error' })
-                }
+                        for (const file of files) {
+                          fs.unlink(
+                            path.join("converted-videos", file),
+                            (err) => {
+                              if (err) {
+                                return res.status(500).send({
+                                  message: "Internal Sevrer error",
+                                  error4: err,
+                                });
+                              }else{
+                                  return res.send({
+                                    message: "Video Uploaded Successfully",
+                                  });
+                              }
+                            }
+                          );
+                        }
+                      });
+
+                    }
+                  }
+                );
+              }
             });
-        }
-    });
-
-    // Creates a raw video in the raw-videos dir
-    fs.writeFile(`./raw-videos/${name}.mp4`, req.files.video.data, "binary", function (err) {
-        if (err) {
-            res.status(500).send({ message: 'Internal Sevrer error' })
-        } else {
-
-            // Reads the raw-videos dir after creation
-            fs.readdir('./raw-videos/', (err, files) => {
-                if (err) {
-                    res.status(500).send({ message: 'Internal Sevrer error' })
-                } else {
-
-                    // converts the raw video to add text and push it to converted-videos dir
-                    files.map(f => {
-                        const command = ffmpeg({ source: `./raw-videos/${f}` })
-                        command
-                            .format('mp4')
-                            .videoFilters({
-                                filter: 'drawtext',
-                                options: {
-                                    text: 'What Is The Longest River In The World ?',
-                                    fontsize: '(h/25)',
-                                    fontcolor: 'white',
-                                    box: 1,
-                                    boxborderw: 10,
-                                    boxcolor: 'black@0.3',
-                                    x: 10,
-                                    y: 10,
-                                }
-                            })
-                            .outputOptions(['-frag_duration 100', '-movflags frag_keyframe+empty_moov', '-pix_fmt yuv420p'])
-                            .output(`converted-videos/${name}.mp4`)
-                            .on('error', (err, stdout, stderr) => {
-                                console.log('err', err.message)
-                                console.log('stdout', stdout)
-                                console.log('stderr', stderr)
-                            })
-                            .on('progress', (progress) => {
-
-                                progress.percent > 99.9 && fs.readdir('./converted-videos', async (err, files) => {
-                                    if (err) {
-                                        res.status(500).send({ message: 'Internal Sevrer error', error: err })
-                                    } else {
-                                        const file = files[files.length - 1]
-                                        fs.readFile(`./converted-videos/${file}`, async (error, item) => {
-                                            if (error) {
-                                                res.status(500).send({ message: 'Internal Sevrer error', error })
-                                            } else {
-                                                const refrence = ref(storage, `${file}`)
-                                                const snap = await uploadBytes(refrence, item)
-                                                const url = await getDownloadURL(ref(storage, snap.ref.fullPath))
-
-                                                await addDoc(collection(database, 'videos'), {
-                                                    media: url,
-                                                    name
-                                                })
-
-                                                res.send({ message: "Video Uploaded Successfully" })
-                                            }
-                                        })
-                                    }
-                                })
-
-                            }).run();
-                    });
-                }
-            })
-        }
-    });
-})
+        })
+        .run();
+    }
+  });
+});
 
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`)
-})
+  console.log(`http://localhost:${port}`);
+});
